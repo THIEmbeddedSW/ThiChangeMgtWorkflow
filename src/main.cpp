1521f563dbf95c0fb4d107c5087a0adeac042ff3
RudolfBauerThalmassing <rudolf.bauer@thi.de>
2025-09-10 10:43:59 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+/**
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+#include <LcdMenu.h>
+#include <MenuScreen.h>
+#include <display/LiquidCrystalAdapter.h>
+#include <renderer/CharacterDisplayRenderer.h>
+
+
+// define the required LCD objects
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+CharacterDisplayRenderer renderer(new LiquidCrystalAdapter(&lcd, 16, 2), 16, 2);
+LcdMenu menu(renderer);
+
+// some strings for displaying the features
+char programVersion[] = "0.0.1";
+char featureString[] = "f: ";
+//char newFeatureA[] = "A";
+//char newFeatureB[] = "B";
+
+/*-----------------------------------------------------------------------------
+ *  Function with new feature
+ -----------------------------------------------------------------------------*
+void fA()
+{
+    strcat(featureString, newFeatureA);
+}
+*/
+
+/*-----------------------------------------------------------------------------
+ *  Function with new feature
+ -----------------------------------------------------------------------------*
+void fB()
+{
+    strcat(featureString, newFeatureB);
+}
+*/
+/*-----------------------------------------------------------------------------
+ *  setup function
+ -----------------------------------------------------------------------------*/
+void setup()
+{
+    renderer.begin();
+    lcd.clear();
+
+	lcd.setCursor(0,0);
+	lcd.print("v");
+	lcd.print(programVersion);
+
+    // integrate new features hereafter
+//   fA();
+//   fB();
+
+    // display new features
+    lcd.setCursor(0,1);
+	lcd.print(featureString);
+}
+
+/*-----------------------------------------------------------------------------
+ *  endless loop
+ -----------------------------------------------------------------------------*/
+void loop()
+{
+}
+
